66c644a1c6db0f631f8e70be505e9863c3b5493e
Jayson <shadowman@shellboot.com>
2018-09-16 23:04:09 +0800

    Divider component

---
+++ src/components/Divider/Divider.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="blockClasses">
+    <span v-if="title" :class="innerClasses">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+import { has } from '../../utils/assist';
+
+const dividerBlockClass = 'h-divider';
+
+export default {
+  name: 'Divider',
+  props: {
+    type: {
+      type: String,
+      default: 'horizontal',
+      validator(value) {
+        return has(value, ['horizontal', 'vertical']);
+      },
+    },
+    title: {
+      type: String,
+    },
+    dashed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    blockClasses() {
+      return [
+        dividerBlockClass,
+        `${dividerBlockClass}--${this.type}`,
+        {
+          [`${dividerBlockClass}--dashed`]: this.dashed,
+        },
+      ];
+    },
+    innerClasses() {
+      return [
+        `${dividerBlockClass}__title`,
+      ];
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../styles/components/divider';
+</style>
---
+++ src/components/Divider/index.js
@@ -0,0 +1,5 @@
+import Divider from './Divider.vue';
+
+
+export default Divider;
+
